56624a4302dacafc84fe8503ce274ae779c03337
Aleš Vomáčka <alesvomacka@seznam.cz>
2022-07-02 20:13:49 +0200

    finished tokenizing text and scrapping names

--- scripts/01-data-cleaning.R
+++ scripts/01-data-cleaning.R
@@ -1,5 +1,8 @@
 # Packages and data -------------------------------------------------------
 library(tidyverse)
+library(rvest)
+library(tidytext)
+library(hunspell)
 
 witcher <- read_file("data-raw/witcher.txt")
 
@@ -13,8 +16,7 @@ books <- map(books, ~as.list(str_split(.x, pattern = "&&&", simplify = TRUE)))
 # Naming books ------------------------------------------------------------
 book_names <- map_chr(books, ~.x[[1]])
 book_names <- str_replace_all(book_names, pattern = "\\n", "")
-book_names <- str_to_lower(book_names)
-book_names <- str_replace_all(book_names, pattern = " ", replacement = "_")
+book_names <- str_squish(book_names)
 names(books) <- book_names
 
 # Naming chapters ---------------------------------------------------------
@@ -22,13 +24,57 @@ books <- map(books, ~.x[-1]) # This is just name of the book
 
 chapter_names <- map(books, ~map(.x, ~str_extract(.x, pattern = ".+\\n\\n")))
 chapter_names <- map(chapter_names, ~map(.x, ~str_replace_all(.x, pattern = "\\n", "")))
-chapter_names <- map(chapter_names, ~map(.x, str_to_lower))
-chapter_names <- map(chapter_names, ~map(.x, ~str_replace_all(.x, pattern = " ", replacement = "_")))
 
 books <- map2(.x = books, .y = chapter_names,
               ~setNames(.x, .y))
 
-# Text cleaning -----------------------------------------------------------
+
+# Cleanining text ---------------------------------------------------------
 # The chapter names are still in the text, let's drop them
 books <- map(books, ~map(.x, ~str_replace(.x, pattern = ".+\\n\\n", replacement = "")))
 
+# sometimes two "new line" symbols appear in the text in the text, we delete them
+books <- map(books, ~map(.x, ~str_replace_all(.x, pattern = "\\n\\n", replacement = " ")))
+
+
+# Tokenizing --------------------------------------------------------------
+books <- books %>% 
+  map(~map(., as_tibble)) %>% 
+  map(~map(., unnest_tokens, input = "value", output = "word")) %>% 
+  map(bind_rows, .id = "chapters") %>% 
+  bind_rows(.id = "book")
+
+books <- books %>% 
+  mutate(word = str_replace_all(word, "’", "'"),
+         chapters = str_to_title(chapters)) %>% 
+  anti_join(stop_words, "word")
+
+books <- books %>% 
+  mutate(stem = hunspell_stem(word, dict = dictionary("en_GB")),
+         stem = map_chr(stem, ~if_else(length(.) > 0,
+                                   .x[1],
+                                   NA_character_)))
+
+# Scrapping character names -----------------------------------------------
+
+characters_pages <- paste0("https://witcher.fandom.com/wiki/Category:",
+                           str_replace_all(book_names, pattern = " ", replacement = "_"),
+                           "_characters")
+
+character_pages <- map(characters_pages, read_html)
+names(character_pages) <- book_names
+
+character_names <- character_pages %>%
+  map(html_elements, ".category-page__member-link") %>% 
+  map(html_text2) %>% 
+  map(as_tibble) %>% 
+  bind_rows(.id = "book")
+
+character_names <- character_names %>% 
+  mutate(first_name = str_extract(value, "^[:alpha:]+")) %>% 
+  rename(full_name = value) %>% 
+  distinct(full_name, .keep_all = TRUE)
+
+# Data export -------------------------------------------------------------
+write_csv(character_names, "data-processed/character-names.csv")
+write_csv(books, "data-processed/witcher-tokenized.csv")
